3e4d72651bf24b9bc1c788ca5ab72566bebe91c3
Arga Revata <36829647+Argarevata@users.noreply.github.com>
2021-01-14 22:06:13 +0700

    maps 70% done

---
+++ Wisata Cianjur Game/Assets/Script/JoystickCon.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class JoystickCon : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler{
+
+	public Image bgImage;
+	public Image joyStickImage;
+	public Vector3 inputVector;
+
+	private void Start()
+	{
+		bgImage = GetComponent<Image> ();
+		joyStickImage = transform.GetChild (0).GetComponent<Image> ();	
+	}
+
+	public virtual void OnDrag(PointerEventData ped)
+	{
+		Vector2 pos;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle (bgImage.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
+			//Debug.Log ("LOL");
+
+			pos.x = (pos.x / bgImage.rectTransform.sizeDelta.x);
+			pos.y = (pos.y / bgImage.rectTransform.sizeDelta.y);
+
+			inputVector = new Vector3 (pos.x*2 , 0, pos.y*2);
+			inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+			joyStickImage.rectTransform.anchoredPosition = new Vector3 (inputVector.x * (bgImage.rectTransform.sizeDelta.x / 3), inputVector.z * (bgImage.rectTransform.sizeDelta.y / 3));
+
+		}
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped)
+	{
+		OnDrag (ped);
+	}
+
+	public virtual void OnPointerUp(PointerEventData ped)
+	{
+		inputVector = Vector3.zero;
+		joyStickImage.rectTransform.anchoredPosition = Vector3.zero;
+	}
+
+}
---
+++ Wisata Cianjur Game/Assets/Script/PlayerCon.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCon : MonoBehaviour {
+
+	public Rigidbody2D myBody;
+	public float speed;
+	public JoystickCon joy;
+	public Animator myAnim;
+
+	public float xAxis, yAxis;
+
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody2D> ();
+		joy = FindObjectOfType<JoystickCon> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//		if (Input.GetKey (KeyCode.A) || joy.inputVector.x >=0.5) {
+		//			myBody.velocity = new Vector2 (-speed, 0);
+		//		}
+		//
+		//		if (Input.GetKey (KeyCode.D)) {
+		//			myBody.velocity = new Vector2 (speed, 0);
+		//		}
+		//
+		//		if (Input.GetKey (KeyCode.W)) {
+		//			myBody.velocity = new Vector2 (0, speed);
+		//		}
+		//
+		//		if (Input.GetKey (KeyCode.S)) {
+		//			myBody.velocity = new Vector2 (0, -speed);
+		//		}
+
+		if (joy.inputVector.x >= 0.7f)
+		{
+			xAxis = 1;
+		}
+		else if (joy.inputVector.x <= -0.7f)
+		{
+			xAxis = -1;
+		}
+		else
+		{
+			xAxis = 0;
+		}
+
+		if (joy.inputVector.z >= 0.7f)
+		{
+			yAxis = 1;
+		}
+		else if (joy.inputVector.z <= -0.7f)
+		{
+			yAxis = -1;
+		}
+		else
+		{
+			yAxis = 0;
+		}
+
+		//myBody.velocity = new Vector2 (joy.inputVector.x * speed, joy.inputVector.z * speed);
+		myBody.velocity = new Vector2(xAxis * speed, yAxis * speed);
+		myAnim.SetFloat("X", xAxis);
+		myAnim.SetFloat("Y", yAxis);
+	}
+}
