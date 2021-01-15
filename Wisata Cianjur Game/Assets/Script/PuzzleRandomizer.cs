097efa73b977d220f3ce5a95ed7f84dbc12d10a2
Arga Revata <36829647+Argarevata@users.noreply.github.com>
2021-01-15 23:16:12 +0700

    + Main Core done

---
+++ Wisata Cianjur Game/Assets/Script/PuzzleRandomizer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleRandomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] puzzlePieces, puzzlePos;
+    public int[] filledPos;
+    private int r;
+    private bool safe;
+
+    public DragController[] thePuzzlePieces;
+    public SpriteRenderer[] puzzlePieceRenderer;
+    public Sprite[] puzzleImage;
+
+    public int whatLevel;
+    public bool win;
+    public GameObject winScreen;
+
+    private void Awake()
+    {
+        RandomizePosition();
+
+        for (int i = 0; i < 16; i++)
+        {
+            puzzlePieceRenderer[i].sprite = puzzleImage[(whatLevel*16)+i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!win)
+        {
+            int x = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                if (thePuzzlePieces[i].locked)
+                {
+                    x++;
+                }
+            }
+            if (x >= 16)
+            {
+                win = true;
+                print("WIN YEAY");
+                winScreen.SetActive(true);
+            }
+        }
+        
+    }
+
+    public void RandomizePosition()
+    {
+        for (int x = 0; x < 16; x++)
+        {
+            filledPos[x] = -1;
+        }
+
+        for (int i = 0; i < 16; i++)
+        {
+            do
+            {
+                r = Random.Range(0, 16);
+                IsNotYetFilled();
+            } while (safe == false);
+            filledPos[i] = r;
+            puzzlePieces[i].transform.position = puzzlePos[r].position;
+        }
+    }
+
+    public void IsNotYetFilled()
+    {
+        safe = true;
+        for (int i = 0; i < 16; i++)
+        {
+            if (r == filledPos[i])
+            {
+                safe = false;
+            }
+        }
+    }
+}
---
+++ Wisata Cianjur Game/Assets/Script/QuizManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public string[] questionPool, answerPool;
+
+    public Text questionText;
+    public Button[] answerButton;
+
+    [HideInInspector]
+    public string[] questions;
+    [HideInInspector]
+    public string[] answer;
+
+    private int currentQuestion;
+
+    public RectTransform[] buttonPos;
+    public int[] filledPos;
+    private int r;
+    private bool safe;
+
+    public GameObject winScreen, loseScreen;
+
+    public int whatLevel;
+
+
+    private void Awake()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            questions[i] = questionPool[(whatLevel * 5) + i];
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            answer[i] = answerPool[(whatLevel * 20) + i];
+        }
+    }
+
+    void Start()
+    {
+        RandomizeBtnPos();
+        currentQuestion = 0;
+        questionText.text = questions[currentQuestion];
+        for (int i = 0; i < 4; i++)
+        {
+            Text answerText = answerButton[i].GetComponentInChildren<Text>();
+            answerText.text = answer[(currentQuestion * 4) + i];
+        }
+    }
+
+    public void Answering(bool x)
+    {
+        if (x)
+        {
+            currentQuestion++;
+            if (currentQuestion < questions.Length)
+            {
+                RandomizeBtnPos();
+                questionText.text = questions[currentQuestion];
+                for (int i = 0; i < 4; i++)
+                {
+                    Text answerText = answerButton[i].GetComponentInChildren<Text>();
+                    answerText.text = answer[(currentQuestion * 4) + i];
+                }
+            }
+            else
+            {
+                print("DONE");
+                winScreen.SetActive(true);
+            }
+        }
+        else
+        {
+            loseScreen.SetActive(true);
+        }
+    }
+
+    public void RandomizeBtnPos()
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            filledPos[x] = -1;    
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            do
+            {
+                r = Random.Range(0, 4);
+                IsNotYetFilled();
+            } while (safe == false);
+            filledPos[i] = r;
+            answerButton[i].transform.position = buttonPos[r].position;
+        }
+    }
+
+    public void IsNotYetFilled()
+    {
+        safe = true;
+        for (int i = 0; i < 4; i++)
+        {
+            if (r == filledPos[i])
+            {
+                safe = false;
+            }
+        }
+    }
+
+    public void LoadScene(string x)
+    {
+        Application.LoadLevel(x);
+    }
+}
